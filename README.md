c2f45d2cbc166cd77bc787d892dcdfe268db868d
Denis Dima <denis.dima@sevio.ro>
2016-12-07 17:36:12 +0200

    Initial Project Deployment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# neteller-ipn
+# NETELLER IPN
+
+Allow neteller payments on your website in PHP. Very Easy to integrate in different shops or websites.
+
+Full POST Back Documentation at:
---
+++ ipn.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Sevio Solutions.
+ * User: Denis DIMA
+ * Product: neteller-ipn
+ * Date: 07.12.2016
+ * Time: 17:21
+ * All rights and copyrights are owned by Sevio Solutions®
+ */
+
+define('SECURE_ID','481496');
