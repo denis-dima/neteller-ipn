2cca70ceadc033735f05f707719362087ce0b8dd
Denis Dima <denis.dima@sevio.ro>
2016-12-07 17:52:27 +0200

    Initial Project Deployment

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
 <body>
 <!--Production: https://api.neteller.com-->
-<form action="https://test.api.neteller.com/v1/oauth2/token" method="POST">
+<form action="https://test.api.neteller.com/v1/oauth2/token" method="GET">
     <input type="hidden" name="grant_type" value="450426945078<?php echo SECURE_ID ?>">
 <!--    <input type="hidden" name="transaction_id" value="--><?php //echo rand(1, 999) ?><!--">-->
 <!--    <input type="hidden" name="return_url" value="http://example.com/finish-url/">-->
